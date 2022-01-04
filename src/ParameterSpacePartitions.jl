8aaae8640f6d7a0f47c66fc81e2f38ff76b1df09
itsdfish <itsdfish@gmail.com>
2022-01-04 14:57:15 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.25

---
+++ src/ParameterSpacePartitions.jl
@@ -0,0 +1,5 @@
+module ParameterSpacePartitions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using ParameterSpacePartitions
+using Test
+
+@testset "ParameterSpacePartitions.jl" begin
+    # Write your tests here.
+end
